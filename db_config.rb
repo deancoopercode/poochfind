ca358303b2b3482dee7b13e58b6392ad624f46ba
Dean Cooper <dean_cooper@hotmail.com>
2016-08-05 12:06:13 +1000

    heroku changes

--- db_config.rb
+++ db_config.rb
@@ -6,4 +6,5 @@ options = {
   database: 'poochfind',
 }
 
-ActiveRecord::Base.establish_connection(options)
+#ActiveRecord::Base.establish_connection(options)
+ActiveRecord::Base.establish_connection( ENV['DATABASE_URL'] || options)
