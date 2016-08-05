2b307ea98ded7ccbea37a7c4342c666c4c8dc46d
Dean Cooper <dean_cooper@hotmail.com>
2016-08-05 11:58:57 +1000

    Tidy up validation and layout some more

--- poochfind.sql
+++ poochfind.sql
@@ -1,17 +1,15 @@
 CREATE DATABASE poochfind;
 
-
 CREATE TABLE users (
   id SERIAL4 PRIMARY KEY,
   email VARCHAR(100) NOT NULL,
   password_digest VARCHAR(400) NOT NULL,
   poochname VARCHAR(100),
   poochbreed VARCHAR(100),
-  poochimageurl VARCHAR(500)
+  poochimageurl VARCHAR(500),
+  username VARCHAR(100),
+  imageurl VARCHAR(500)
 );
-
-ALTER TABLE users add column username VARCHAR(100);
-ALTER TABLE users add column imageurl VARCHAR(500);
 
 CREATE TABLE friendships (
   id SERIAL4 PRIMARY KEY,
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,14 +1,67 @@
-function validateSubmitForm() {
+function validateSubmitSignupForm() {
 
-    var x = document.getElementById("username").value;
-    if (x == null || x == "") {
-        alert("Username must be filled out");
-        return false;
+    var missingFields = "";
+
+    if (document.getElementById("username").value == "") {
+        missingFields += "\nUsername";
     }
-    var x = document.getElementById("password").value;
-    if (x == null || x == "") {
-        alert("Password must be filled out");
-        return false;
+    if (document.getElementById("password").value == "") {
+        missingFields += "\nPassword";
+    }
+    if (document.getElementById("imageurl").value == "") {
+        missingFields += "\nImage URL";
+    }
+    if (document.getElementById("poochname").value == "") {
+        missingFields += "\nPooch Name";
+    }
+    if (document.getElementById("poochbreed").value == "") {
+        missingFields += "\nPooch Breed";
+    }
+    if (document.getElementById("poochimageurl").value == "") {
+        missingFields += "\nPooch Image URL";
+    }
+
+    if (missingFields == "") {
+      return true;
+    }
+    else {
+      alert("Some fields are missing: " + missingFields);
+      return false;
+    }
+
+}
+
+
+function validateSubmitCommentForm() {
+
+    var missingFields = "";
+
+    if (document.getElementById("newCommentText").value == "") {
+      alert("You didnt enter a comment");
+      return false;
     }
     return true;
+
+}
+
+
+
+function validateSubmitPostForm() {
+
+    var missingFields = "";
+
+    if (document.getElementById("textarea").value == "") {
+        missingFields += "\nComments";
+    }
+    if (document.getElementById("primaryimageurl").value == "") {
+        missingFields += "\nImage URL/Video";
+    }
+    if (missingFields == "") {
+      return true;
+    }
+    else {
+      alert("Some fields are missing: " + missingFields);
+      return false;
+    }
+
 }
